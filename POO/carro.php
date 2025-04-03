7874f6eca1e3019fd26565c1e02f8aab9667a2e6
freitasrafa <freitaxxrafah@gmail.com>
2025-04-03 15:47:03 -0300

    org arquivos

---
+++ POO/carro.php
@@ -0,0 +1,45 @@
+<?php 
+
+    // Criando uma classe
+    class Carro{
+    
+        // Declaração de variaveis, atributos 
+        public $modelo;
+
+        public $fabricantes;
+
+        public $anoModelo;
+
+        // Declaração de ações, métodos
+        public function acelerar(){
+
+            echo("Veículo acelerado!");
+
+        }
+
+    }
+
+    // Instanciando o objeto aos 'moldes' da classe carro
+    // Sintaxe: $nomeObjeto = new MetodoConstrutor(); --> mesmo nome da classe
+    $meuCarro = new Carro();
+
+    // Gravando os valores dentro dos atributos do meu objeto
+    $meuCarro->modelo           = "UNO";
+
+    $meuCarro->fabricante       = "FIAT";
+
+    $meuCarro->anoModelo        =  2014;
+
+    // Recuperando valores internos do atributo
+    echo("Modelo do carro: "  . $meuCarro->modelo     . "<br/>");
+
+    echo("Fabricante: "       . $meuCarro->fabricante . "<br/>");
+
+    echo("Ano do modelo: "    . $meuCarro->anoModelo  . "<br/>");
+
+    // Invocar um método (ação do objeto)
+    $meuCarro->acelerar();
+
+    
+
+?>
---
+++ POO/carro_private.php
@@ -0,0 +1,88 @@
+<?php 
+
+    // Criando uma classe
+    class Carro{
+    
+        // Declaração de variaveis, atributos 
+        private $modelo;
+
+        // Métodos de acesso
+
+        // lê o modelo
+        public function setModelo($valor){
+
+            $this->modelo = $valor;
+
+        }
+
+        // Retorna o modelo
+        public function getModelo(){
+
+            return $this->modelo;
+        }
+
+        // Lê o fabricante
+        public function setFabricante($valor){
+
+            $this->fabricante = $valor;
+
+        }
+
+        // Retorna o fabricante
+        public function getFabricante(){
+            
+            return $this->fabricante;
+
+        }
+
+         // Lê o ano do modelo
+        public function setAnoModelo($valor){
+
+            $this->anoModelo = $valor;
+
+        }
+
+        // Retorna o ano do modelo
+        public function getAnoModelo(){
+
+            return $this->anoModelo;
+
+        }
+
+        public $fabricantes;
+
+        public $anoModelo;
+
+        // Declaração de ações, métodos
+        public function acelerar(){
+
+            echo("Veículo acelerado!");
+
+        }
+
+    }
+
+    // Instanciando o objeto aos 'moldes' da classe carro
+    // Sintaxe: $nomeObjeto = new MetodoConstrutor(); --> mesmo nome da classe
+    $meuCarro = new Carro();
+
+    // Gravando os valores dentro dos atributos do meu objeto
+    $meuCarro->setModelo("UNO");
+
+    $meuCarro->setFabricante("FIAT");
+
+    $meuCarro->setAnoModelo(2014);
+
+    // Recuperando valores internos do atributo
+    echo("Modelo do carro: "  . $meuCarro->getModelo()      . "<br/>");
+
+    echo("Fabricante: "       . $meuCarro->getFabricante()  . "<br/>");
+
+    echo("Ano do modelo: "    . $meuCarro->getAnoModelo()   . "<br/>");
+
+    // Invocar um método (ação do objeto)
+    $meuCarro->acelerar();
+
+    
+
+?>
